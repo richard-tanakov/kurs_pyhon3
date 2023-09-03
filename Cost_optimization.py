cd3d3704a69f079e649c85726126cedaae364043
Rich <rich@trivium.nohost.me>
2023-09-03 02:44:04 -0500

    Cost_optimization.py

--- Cost_optimization.py
+++ Cost_optimization.py
@@ -1,23 +1,28 @@
-import decimal 
+"""10 % доходов идут на отпуск
+30 % доходов на пропитание и еду
+5 % на коммунальные платежи
+15 % на выходной досуг
+остальные 40% идут в накопления"""
+
+income_girl = float(input()) * 100
+income_man = float(input()) * 100  # На вход подаётся доход двух людей.
+income = int(income_girl + income_man)
 
 
-income= (30000.50+20000.75) # По условиям задачи подаётся два дохода. 
-
-vacation=(income*10/100) #Отпуск
-needs=(income*30/100)    #Нужды
-apartment=(income*5/100)   #Комуналка
-leisure=(income*15/100) #Досуг
-savings=(income*40/100)  #Накопления
-
-
-def penny (vacation,needs,apartment,leisure,savings): # Вычисление копеек
-    vacation_penny= str(vacation-int(vacation))[+2:5] # [+2:4] Выводить две цифры после точки
-    needs_penny=str(needs-int(needs))[+2:4]
-    apartment_penny=str(apartment-int(apartment))[+2:4]
-    leisure_penny=str(leisure-int(leisure))[+2:4]
+def whole_sum_calculation(income):  # Для того чтобы вывести копейки нужно результаты функции %100, а целую часть на //100. Сделаем на выводе.
     
-    conclusion(vacation,)
+    vacation = (10 * income // 100)   # Отпуск
+    needs = (30 * income // 100)   # Нужды
+    communal = (5 * income // 100)   # Коммуналка
+    leisure = (15 * income // 100)   # Досуг
+    saving= income-(vacation+needs+communal+leisure)
+    withdrawal (income,vacation, needs, communal, leisure,saving)
 
-penny (vacation,needs,apartment,leisure,savings)
+def withdrawal (income,vacation, needs, communal, leisure,saving):
+    print("Отпуск:", (vacation//100), 'руб.', (vacation%100), 'коп.')
+    print("Пропитание и еда:",(needs//100),'руб.',(needs%100), 'коп.')
+    print('Коммунальные платежи:',(communal//100),'руб.',(communal%100), 'коп.')
+    print('Досуг:',(leisure//100),'руб.',(leisure%100), 'коп.')
+    print('Накопления:',(saving//100),'руб.',(saving%100), 'коп.')
 
-
+whole_sum_calculation(income) # запуск расчётов
