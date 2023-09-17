2248d6c59b50ab4ea7623e5ae56a224045a0c10e
Rich <rich@trivium.nohost.me>
2023-09-17 02:52:10 -0500

    Update Conditions_of_expression

---
+++ Conditions_of_expression/Even_Odd.py
@@ -0,0 +1,6 @@
+num= int(input())
+if num%2 !=0:
+    print(num-10)
+else:
+    print(num+10)
+    
---
+++ Conditions_of_expression/Print_the_smaller_number.py
@@ -0,0 +1,5 @@
+num1, num2 = int(input()), int(input())
+if num1 > num2:
+    print(num2)
+else:
+    print(num1)
