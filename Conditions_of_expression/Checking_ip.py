5c6984b2af498b1e013ba62cb181be30110736c5
Rich <rich@trivium.nohost.me>
2023-09-21 03:02:43 -0500

    Update Conditions

---
+++ Conditions_of_expression/Checking_ip.py
@@ -0,0 +1,16 @@
+n1, n2, n3, n4 = [int(i) for i in input().split(".")]
+
+
+def out(n1, n2, n3, n4):
+    if n1 == 0 and n2 == 0 and n3 == 0 and n4 == 0:
+        print("False")
+    elif n1 == 255 and n2 == 255 and n3 == 255 and n4 == 255:
+        print("False")
+    else:
+        print("True")
+
+
+if n1>255 or n2 >255 or n3> 255 or n4>255:
+        print("False")
+else:
+    out(n1, n2, n3, n4)
