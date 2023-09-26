77ac4de15035a7b89ebfca882ed2661d017b1c5f
Rich <rich@trivium.nohost.me>
2023-09-26 16:23:43 -0500

    Update Conditions_of_expression

---
+++ Conditions_of_expression/CountingStringLengths.py
@@ -0,0 +1,15 @@
+(
+    str1,
+    str2,
+    str3,
+) = (
+    str(input()),
+    str(input()),
+    str(input()),
+)
+if len(str1) == len(str2) and (len(str2) != len(str3)): 
+    print(str2)
+elif (len(str1) == len(str3)) or (len(str2) == len(str3)):
+    print(str3)
+else:
+    print(min(str1, str2, str3, key=len)) # Вывод самой минимальной строки. 
---
+++ Conditions_of_expression/match_case.py
@@ -0,0 +1,6 @@
+match dialogue:= str(input())
+    case dialogue=='Привет':
+        print('Привет!')
+    case dialogue=='Как дела?' 
+        print('Все классно!')    
+    case dialogue=='Пока'
